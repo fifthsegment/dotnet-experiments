464bd4976d049626471c93b9cc91ca596177c531
Abdullah Irfan <abdullah.irfan1@gmail.com>
2024-04-22 00:24:42 +0200

    delete pods test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return 'Hello, World from k8s! ! ! ! !'
+    return 'Hello! from k8s'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
