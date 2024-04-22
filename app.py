c14d8493f60a0850bdfcb788fa3e71f0fbdd0aa7
fifthsegment <abdullah.irfan1@gmail.com>
2024-04-22 17:08:39 +0200

    test2

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return 'Hello! from k8s'
+    return 'Hello! from k8s.'
 
 @app.route('/world')
 def world():
-    return 'World! from k8s'
+    return 'World! from k8s.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
