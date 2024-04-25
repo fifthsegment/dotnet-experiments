f204c0339798882a6d834b5545bc29bf15d0abe3
Abdullah Irfan <abdullah.irfan1@gmail.com>
2024-04-25 22:32:42 +0200

    update deployer again

--- deployer.sh
+++ deployer.sh
@@ -2,10 +2,13 @@
 export DOTNET_ROOT=/home/abdullah/.dotnet
 export PATH=$PATH:/home/abdullah/.dotnet:/home/abdullah/.dotnet/tools
 set -e  # Exit immediately if any command fails
+read -r password < ./temp_pass.txt
+rm -rf temp_pass.txt
 cd AspireProject/AspireProject.AppHost
+echo "READING CREDENTIALS"
 echo "BUILD MANIFEST"
 aspirate build --non-interactive -m ./manifest.json
 echo "GENERATE KUBERNETES FILES"
-aspirate generate --non-interactive -m ./manifest.json --skip-build --secret-password "$ASPIRE_PASSWORD" --image-pull-policy IfNotPresent --include-dashboard true
+aspirate generate --non-interactive -m ./manifest.json --skip-build --secret-password "$password" --image-pull-policy IfNotPresent --include-dashboard true
 echo "APPLYING CONFIG"
-aspirate apply --non-interactive --secret-password "$ASPIRE_PASSWORD" -k docker-desktop
+aspirate apply --non-interactive --secret-password "$password" -k docker-desktop
