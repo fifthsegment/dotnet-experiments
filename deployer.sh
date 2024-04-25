e1adaae27454b86d6080aa6ada431a4fc607d825
Abdullah Irfan <abdullah.irfan1@gmail.com>
2024-04-25 23:44:46 +0200

    update deployment

--- deployer.sh
+++ deployer.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 export DOTNET_ROOT=/home/abdullah/.dotnet
 export PATH=$PATH:/home/abdullah/.dotnet:/home/abdullah/.dotnet/tools
+
 set -e  # Exit immediately if any command fails
 read -r password < ./temp_pass.txt
 rm -rf temp_pass.txt
+mv deployment.yml AspireProject/AspireProject.AppHost/external.yml
 cd AspireProject/AspireProject.AppHost
 echo "READING CREDENTIALS"
 echo "BUILD MANIFEST"
 aspirate build --non-interactive -m ./manifest.json
 echo "GENERATE KUBERNETES FILES"
 # Ignoring helm errors
-set +e
 aspirate generate --non-interactive -m ./manifest.json --skip-build --secret-password "$password" --image-pull-policy IfNotPresent --include-dashboard true -sh
-set -e
+echo "INJECTING LOCAL CONFIG"
+sed -i "/resources:/a- external.yml" "kustomization.yml"
 echo "APPLYING CONFIG"
 aspirate apply --non-interactive --secret-password "$password" -k docker-desktop
