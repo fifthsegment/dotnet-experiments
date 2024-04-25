e3210a486566640b9c399ff47fbedbac7b436d71
Abdullah Irfan <abdullah.irfan1@gmail.com>
2024-04-25 08:54:45 +0200

    update deployer

--- deployer.sh
+++ deployer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export DOTNET_ROOT=/home/abdullah/.dotnet
 export PATH=$PATH:/home/abdullah/.dotnet:/home/abdullah/.dotnet/tools
-password=$ENV_KUBERNETES_PASSWORD
+set -e  # Exit immediately if any command fails
 cd AspireProject/AspireProject.AppHost
 aspirate build --non-interactive -m ./manifest.json
-aspirate generate --non-interactive -m ./manifest.json --skip-build --secret-password "$password" --image-pull-policy IfNotPresent --include-dashboard true
-aspirate apply --non-interactive --secret-password "$password" -k docker-desktop
+aspirate generate --non-interactive -m ./manifest.json --skip-build --secret-password "$ASPIRE_PASSWORD" --image-pull-policy IfNotPresent --include-dashboard true
+aspirate apply --non-interactive --secret-password "$ASPIRE_PASSWORD" -k docker-desktop
